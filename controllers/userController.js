bfcd985db8631ab8521119da91da5746d218a67f
Khiran Gannu <117846037+Mangocod3r@users.noreply.github.com>
2024-08-18 09:05:29 +0530

    Update userController.js

--- controllers/userController.js
+++ controllers/userController.js
@@ -39,4 +39,20 @@ const signupUser = async (req, res) => {
   }
 }
 
-module.exports = { signupUser, loginUser }
+const getUserDetails = async (req, res) => {
+  const { email } = req.params
+
+  try {
+    const user = await User.findOne({ email })
+
+    if (!user) {
+      return res.status(404).json({ error: 'User not found' })
+    }
+
+    res.status(200).json({ name: user.name, email: user.email, role: user.role })
+  } catch (error) {
+    res.status(400).json({ error: error.message })
+  }
+}
+
+module.exports = { signupUser, loginUser, getUserDetails }
